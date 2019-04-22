6af6e8a38b2f795a16e0b9ba6775a513218da55e
Philip Wright <wrightphilip4@gmail.com>
2019-04-22 17:36:50 +0100

    Adding the Caesar algorithm

---
+++ caesar.py
@@ -0,0 +1,29 @@
+import sys
+
+if len(sys.argv) == 2:
+    skey = sys.argv[1]
+    key = int(skey)
+else:
+    print("You must enter a key")
+    sys.exit(1)
+
+p = input("Please insert plaintext")
+
+print("ciphertext: ", end="")
+
+for i in range(len(p)):
+    if p[i].isalpha():
+        if p[i].isupper():
+            letter = ord(p[i]) - 65
+            encrypt = ((letter + key) % 26) + 65
+            cencrypt = chr(encrypt)
+            print(f"{cencrypt}", end="")
+        elif p[i].islower():
+            letter = ord(p[i]) - 97
+            encrpt = ((letter + key) % 26) + 97
+            cencrypt = chr(encrypt)
+            print(f"{cencrypt}", end="")
+        else:
+            print(f"{p[i]}", end="")
+
+print()
